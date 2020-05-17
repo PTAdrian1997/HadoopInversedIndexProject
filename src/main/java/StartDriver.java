e5de9fcd3c98482080f942f981f70fb005627f0e
PTAdrian1997 <poptifreaadrian@gmail.com>
2020-05-17 22:26:25 +0300

    update the documentation of the StartDriver class

--- src/main/java/StartDriver.java
+++ src/main/java/StartDriver.java
@@ -2,17 +2,29 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+/**
+ * This here is the class that starts everything (The Driver class).
+ */
 public class StartDriver {
 
     public static void main(String[] args) throws Exception {
 
-        String stopWordsFile = args[0];
+        String stopWordsFilePath = args[0];
+        String inputFilePath = args[1];
+        String outputFilePath = args[2];
 
+        // configure the Job:
         Job job = Job.getInstance(new Configuration(), "inversed index");
+        job.setJarByClass(StartDriver.class);
 
         // add the stop-words file to be localize:
-        job.addCacheFile(new Path(stopWordsFile).toUri());
+        job.addCacheFile(new Path(stopWordsFilePath).toUri());
 
+        FileInputFormat.addInputPath(job, new Path(inputFilePath));
+        FileOutputFormat.setOutputPath(job, new Path(outputFilePath));
         // Submit the job to the cluster:
         job.waitForCompletion(true);
     }
