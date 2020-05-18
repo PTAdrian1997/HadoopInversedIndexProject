3f98ad067a7d56f0c40d7bb494e751202fc60feb
PTAdrian1997 <poptifreaadrian@gmail.com>
2020-05-18 16:05:54 +0300

    take the input filename from the context

--- src/main/java/mapper/InversedIndexMapper.java
+++ src/main/java/mapper/InversedIndexMapper.java
@@ -1,39 +1,38 @@
 package mapper;
 
-import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class InversedIndexMapper
-        extends MapReduceBase
-        implements Mapper<Object, Text, Text, InversedIndexRecord> {
+        extends Mapper<Object, Text, Text, Text> {
 
-//    public void map(Object key, Text value, Context context)
-//    throws IOException, InterruptedException {
-//        // get the filename:
-//        String filename = ((FileSplit)context.getInputSplit()).getPath().getName();
-//        // split the words:
-//        String separators = "\"\',.()?![]#$*+-;:_/\\<>@%& ";
-//        StringTokenizer stringTokenizer = new StringTokenizer(value.toString(), separators);
-//        while (stringTokenizer.hasMoreElements()){
-//            String currentWord = stringTokenizer.nextToken();
-//
-//        }
-//    }
+    private File stopwordsFile;
+    private Text word = new Text();
+    private Text recordText = new Text();
 
-    public void map(Object o, Text text, OutputCollector<Text, InversedIndexRecord> outputCollector, Reporter reporter) throws IOException {
-        // get the filename:
+    public void map(Object o, Text text, Context context) throws IOException, InterruptedException {
+        // get the inputFilename:
+        String inputFileName = ((FileSplit)context.getInputSplit()).getPath().getName();
+        // get the line number:
+        // split the words:
+        String separators = "\"\',.()?![]#$*+-;:_/\\<>@%& ";
+        StringTokenizer stringTokenizer = new StringTokenizer(text.toString(), separators);
 
+        while (stringTokenizer.hasMoreElements()){
+            String currentWord = stringTokenizer.nextToken();
+            InversedIndexRecord currentRecord = new InversedIndexRecord(
+                    inputFileName, 0L, 0L
+            );
+            word.set(currentWord);
+            recordText.set(currentRecord.toString());
+            context.write(word, recordText);
+        }
     }
 
-    public void close() throws IOException {
-
-    }
-
-    public void configure(JobConf jobConf) {
-        // get the cached stop words file:
-    }
 }
--- src/main/java/mapper/InversedIndexRecord.java
+++ src/main/java/mapper/InversedIndexRecord.java
@@ -1,10 +1,21 @@
 package mapper;
 
-public class InversedIndexRecord {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 
-    private String filename = null;
-    private Long lineNumber = null;
-    private Long wordNumber = null;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class InversedIndexRecord implements Writable {
+
+    private String filename;
+    private Long lineNumber;
+    private Long wordNumber;
+
+    public InversedIndexRecord(){}
 
     public InversedIndexRecord(String filename, Long lineNumber, Long wordNumber){
         this.filename = filename;
@@ -23,4 +34,29 @@ public class InversedIndexRecord {
     public Long getWordNumber(){
         return this.wordNumber;
     }
+
+    public String toString(){
+        return "InversedIndexRecord[filename=" + this.filename +
+                ", line_number=" + this.lineNumber +
+                ", word_number=" + this.wordNumber + "]";
+    }
+
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeChars(this.filename);
+        dataOutput.writeLong(this.lineNumber);
+        dataOutput.writeLong(this.wordNumber);
+    }
+
+    public void readFields(DataInput dataInput) throws IOException {
+        this.filename = dataInput.readLine();
+        this.lineNumber = dataInput.readLong();
+        this.wordNumber = dataInput.readLong();
+    }
+
+    public static InversedIndexRecord read(DataInput in) throws IOException {
+        InversedIndexRecord newRecord = new InversedIndexRecord();
+        newRecord.readFields(in);
+        return newRecord;
+    }
+
 }
